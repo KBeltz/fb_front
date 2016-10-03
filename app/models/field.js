1c312f6ef9b641e69b039ececa4ad2548e868bfa
kasey mccormick <mccormick.kasey@gmail.com>
2016-10-03 13:46:14 -0500

    generated field resources

---
+++ app/models/field.js
@@ -0,0 +1,11 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  fieldName: DS.attr('string'),
+  updateBy: DS.attr('number'),
+  insertBy: DS.attr('number'),
+  updateTime: DS.attr('date'),
+  insertTime: DS.attr('date'),
+  submissions: DS.hasMany('submission'),
+  templateFields: DS.hasMany('template-field')
+});
