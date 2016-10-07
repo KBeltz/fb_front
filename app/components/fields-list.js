6e85948bfbac391129fc6abf71f1fa8075ca2922
skyler.sidner <skyler.sidner@Skylers-MacBook-Pro.local>
2016-10-07 11:23:16 -0500

    updated component to be indepent of the template

--- app/components/fields-list.js
+++ app/components/fields-list.js
@@ -5,6 +5,17 @@ export default Ember.Component.extend({
     
   },
   
+  store: Ember.inject.service(),
+  
+  fields: null,
+
+  didInsertElement() {
+    this._super(...arguments);
+    this.get('store').findAll('field').then((fields) => {
+       this.set('fields', fields);
+    });
+  },
+  
   showMe: false,
   
   actions: {
