5e2c7115e4529e85a506e585022a8c29c48d08bb
KBeltz <beltz08@me.com>
2016-10-10 11:42:54 -0500

    Add send-template route and modify router path

--- app/router.js
+++ app/router.js
@@ -17,9 +17,11 @@ Router.map(function() {
   this.route('user', { path: 'users/:user_id' });
   this.route('user-forms');
   this.route('template-show', { path: 'templates/:template_id' });
+  this.route('send-template', { path: 'templates/:template_id/send-template' });
   this.route('form', { path: 'completed-forms/:form_id' });
   // wildcard route
   this.route('page-not-found', { path: '/*wildcard' });
+  this.route('send-template');
 });
 
 export default Router;
