9a030d376d58367e9784ae25b118b40374c5e2a5
KBeltz <beltz08@me.com>
2016-10-10 11:54:31 -0500

    Update send-template route

--- app/router.js
+++ app/router.js
@@ -17,11 +17,10 @@ Router.map(function() {
   this.route('user', { path: 'users/:user_id' });
   this.route('user-forms');
   this.route('template-show', { path: 'templates/:template_id' });
-  this.route('send-template', { path: 'templates/:template_id/send-template' });
+  this.route('template.send-template', { path: 'templates/:template_id/send-template' });
   this.route('form', { path: 'completed-forms/:form_id' });
   // wildcard route
   this.route('page-not-found', { path: '/*wildcard' });
-  this.route('send-template');
 });
 
 export default Router;
