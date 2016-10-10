ecb5300490c42e6b415b3dcbcc8eec15354b8ba9
kasey mccormick <mccormick.kasey@gmail.com>
2016-10-10 14:26:04 -0500

    removing console.log

--- app/routes/form.js
+++ app/routes/form.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    console.log("HERE look HERE for params" + params["form_id"])
     return this.get('store').find('form', params["form_id"]);
   }
 });
