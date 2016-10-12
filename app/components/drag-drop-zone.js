58278c157f0cebe41afaf8bf3fce81413d37f048
skyler.sidner <skyler.sidner@Skylers-MacBook-Pro.local>
2016-10-12 15:25:11 -0500

    working drag and drop demo with incrementing of a component attribute

---
+++ app/components/drag-drop-zone.js
@@ -0,0 +1,25 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  classNames: ['draggable-dropzone'],
+  classNameBindings: ['dragClass'],
+  dragClass: 'deactivated',
+  dragLeave(event) {
+    event.preventDefault();
+    return this.set('dragClass', 'deactivated');
+  },
+  dragOver(event) {
+    event.preventDefault();
+    return this.set('dragClass', 'activated');
+  },
+  drop(event) {
+    var data;
+    this.set('dragClass', 'deactivated');
+    data = event.dataTransfer.getData('text/data');
+    event.target.appendChild(document.getElementById(data));
+    this.set('dropCounter', this.get('dropCounter') + 1);
+  },
+  
+  dropCounter: 0
+  
+});
