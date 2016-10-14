638a4fbcb875c24f0c3831423f2a398b346d6608
Chris Foral <chris.foral@atssecured.com>
2016-10-14 08:56:39 -0500

    Created universal error template. Implemented conditionals based on current payload. Need to configure serializer to get server errors in JSON API format

--- app/routes/application-error.js
+++ app/routes/application-error.js
@@ -2,6 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   setupController(controller, error) {
+    let title = 'You have died of dysentery.';
+    // let code = error.code;
+    if (error) {
+      if (error.isAdapterError) {
+        title = 'Oops, there was an adapter error…';
+      } else {
+        title = "Opps, can't find this one…";
+      }
+      controller.set('title', title);
+    }
     this._super(controller, error);
+    // {{debugger}}
   }
 });
