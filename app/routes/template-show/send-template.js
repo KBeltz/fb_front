984af39aedb8b54c9fd4de1940b75f6bf96a5ea1
KBeltz <beltz08@me.com>
2016-11-02 11:49:59 -0500

    Update send-template route handler with rsvp hash. Pass user id to sendTemplate action

--- app/routes/template-show/send-template.js
+++ app/routes/template-show/send-template.js
@@ -1,15 +1,21 @@
 import Ember from 'ember';
+import RSVP from 'rsvp';
 
 export default Ember.Route.extend({
   model() {
-    return this.get('store').findAll('user', {reload: true}).then(users => users.sortBy('lastName', 'firstName'));
+    return RSVP.hash ({
+      user: this.get('store').findAll('user', {reload: true}).then(users => users.sortBy('lastName', 'firstName')),
+      template: this.get('store').findAll('template'),
+      form: this.store.createRecord('form')
+    });
   },
   renderTemplate() {
     this.render('template-show.send-template', { into: 'application' });
   },
   actions: {
-    sendTemplate() {
+    sendTemplate(user) {
       console.log("send template action");
+      console.log(user.id);
     }
   }
 });
