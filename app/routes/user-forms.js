3dd1b039b9339e7295a75d253b4ac22fe0316254
KBeltz <beltz08@me.com>
2016-11-04 12:14:13 -0500

    Return alphebetized list

--- app/routes/user-forms.js
+++ app/routes/user-forms.js
@@ -4,6 +4,6 @@ export default Ember.Route.extend({
   model() {
     return this.get('store').query('user', {
       isComplete: 'N'
-    });
+    }).then(users => users.sortBy('lastName', 'firstName'));
   }
 });
